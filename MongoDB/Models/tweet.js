28a08c4796c0d5496cd99affdf61c23d7170d14c
Imshyeon <kzoen0040@naver.com>
2023-10-24 12:05:41 +0900

    bajillions-2

--- MongoDB/Models/tweet.js
+++ MongoDB/Models/tweet.js
@@ -26,15 +26,18 @@ const User = mongoose.model('User', userSchema)
 const Tweet = mongoose.model('Tweet', tweetSchema)
 
 const makeTweets = async () => {
-    const user = new User({ username: 'bradpitt', age: 36 })
-    const tweet1 = new Tweet({ text: 'I will go to Korea.', likes: 23528 })
-    tweet1.user = user
-    user.save()
-    tweet1.save()
+    // const user = new User({ username: 'bradpitt', age: 36 })
+    const user = await User.findOne({ username: 'bradpitt' })
+    // const tweet1 = new Tweet({ text: 'I will go to Korea.', likes: 23528 })
+    const tweet2 = new Tweet({ text: 'Hello Korea! I am Here now!', likes: 2349825 })
+    tweet2.user = user
+    // user.save()
+    tweet2.save()
 }
 makeTweets()
 
 // 몽고 쉘
+// 예제 1.
 // relationshopDemo> db.tweets.find()
 // [
 //   {
@@ -42,6 +45,56 @@ makeTweets()
 //     text: 'I will go to Korea.',
 //     likes: 23528,
 //     user: ObjectId("6537329ce6364d5a183cf0b5"),
+//     __v: 0
+//   }
+// ]
+
+// 예제 2.
+// relationshopDemo> db.tweets.find()
+// [
+//   {
+//     _id: ObjectId("6537329ce6364d5a183cf0b6"),
+//     text: 'I will go to Korea.',
+//     likes: 23528,
+//     user: ObjectId("6537329ce6364d5a183cf0b5"),
+//     __v: 0
+//   },
+//   {
+//     _id: ObjectId("6537334a812c4346b35ef128"),
+//     text: 'Hello Korea! I am Here now!',
+//     likes: 2349825,
+//     user: ObjectId("6537329ce6364d5a183cf0b5"),
+//     __v: 0
+//   }
+// ]
+
+
+const findTweets = async () => {
+    const t = await Tweet.find({})
+        .populate('user', 'username')
+    console.log(t)
+}
+findTweets()
+
+// [
+//  {
+//     _id: new ObjectId("653733d43575878255bf3cc6"),
+//     text: 'Hello Korea! I am Here now!',
+//     likes: 2349825,
+//     user: {
+//       _id: new ObjectId("6537329ce6364d5a183cf0b5"),
+//       username: 'bradpitt'
+//     },
+//     __v: 0
+//   },
+//   {
+//     _id: new ObjectId("65373402ec9c433b9ebbc8fc"),
+//     text: 'Hello Korea! I am Here now!',
+//     likes: 2349825,
+//     user: {
+//       _id: new ObjectId("6537329ce6364d5a183cf0b5"),
+//       username: 'bradpitt'
+//     },
 //     __v: 0
 //   }
 // ]
