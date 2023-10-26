9f3587fcf9c1c25ca0bd22ac00f34a4332a6dc43
Imshyeon <kzoen0040@naver.com>
2023-10-26 20:34:08 +0900

    mongoose middleware - delete

--- Node/Node_10/models/farm.js
+++ Node/Node_10/models/farm.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const Product = require('./product')
 const { Schema } = mongoose
 const farmSchema = new Schema({
     name: {
@@ -20,5 +21,13 @@ const farmSchema = new Schema({
     ]
 })
 
-const Farm = mongoose.model('Farm',farmSchema)
+// mongoose middleware
+farmSchema.post('findOneAndDelete', async function (farm) {
+    if (farm.products.length) {
+        const res = await Product.deleteMany({ _id: { $in: farm.products } })
+        console.log(res)
+    }
+})
+
+const Farm = mongoose.model('Farm', farmSchema)
 module.exports = Farm
