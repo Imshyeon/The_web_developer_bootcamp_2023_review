f89fe508b2f8da5ad9ba4bfb0b8bc9517349c313
Imshyeon <kzoen0040@naver.com>
2023-10-29 15:49:48 +0900

    cookie

--- Node/Node_11_RouterDemo/index.js
+++ Node/Node_11_RouterDemo/index.js
@@ -5,9 +5,20 @@ const dogsRoutes = require('./routes/dogs')
 const adminRoutes = require('./routes/admin')
 
 app.use('/shelters', shelterRoutes)
-app.use('/dogs',dogsRoutes)
-app.use('/admin',adminRoutes)
+app.use('/dogs', dogsRoutes)
+app.use('/admin', adminRoutes)
 
-app.listen(3000, ()=>{
+//==========cookie==========
+app.get('/greet', (req, res) => {
+    res.send('hey there')
+})
+
+app.get('/setname', (req, res) => {
+    res.cookie('name', 'suhyeon kang')
+    res.cookie('animal', 'kitten')
+    res.send('ok sent your a cookie')
+})
+
+app.listen(3000, () => {
     console.log('Serving app on localhost:3000')
 })
