64e095c2dabba8674dec4c8dc5d89af2d45363df
Imshyeon <kzoen0040@naver.com>
2023-10-30 17:07:27 +0900

    서명된 쿠키 | signed cookie

--- Node/Node_11_RouterDemo/README.md
+++ Node/Node_11_RouterDemo/README.md
@@ -3,3 +3,8 @@
 
 # 쿠키(http cookies)
 사용자의 브라우저에 저장할 수 있는 아주 작은 정보 조각. 특정 웹 페이지와 연관되어 있다.
+
+- signed 쿠키
+  1. fruit : grape로 설정. 그러나 웹 페이지의 Application에서 수동으로 value를 apple 로 설정한다면, signed cookie가 나타나지 않음.
+  &rarr; 누군가 바꿨음을 알 수 있음.
+  2. 만약 비밀 키를 바꿨을 경우, 그 비밀 키로 서명되었던 모든 쿠키가 무효화.
--- Node/Node_11_RouterDemo/index.js
+++ Node/Node_11_RouterDemo/index.js
@@ -11,7 +11,7 @@ app.use('/shelters', shelterRoutes)
 app.use('/dogs', dogsRoutes)
 app.use('/admin', adminRoutes)
 
-app.use(cookieParser())
+app.use(cookieParser('thisismysecret'))
 
 //==========cookie==========
 app.get('/greet', (req, res) => {
@@ -24,6 +24,19 @@ app.get('/setname', (req, res) => {
     res.cookie('name', 'suhyeon kang')
     res.cookie('animal', 'kitten')
     res.send('ok sent your a cookie')
+})
+
+app.get('/getsignedcookie',(req,res)=>{
+    res.cookie('fruit','grape',{signed:true})
+    res.send('ok signed your fruit cookie') // value : s%3Agrape.LMNZojp%2FiR9Tsj50P0ysA22deJjrP0awUK0S8R3lTUk
+    // 무결성, 유효성 확인. 아무도 건드리지 않고, 아무것도 바뀌지 않음을 확인
+})
+
+app.get('/verifyfruit',(req,res)=>{
+    console.log(req.cookies)
+    console.log(req.signedCookies)
+    // res.send(req.cookies) // fruit와 관련된 쿠키는 보이지 않음.
+    res.send(req.signedCookies) // fruit와 관련된 쿠키가 보임.
 })
 
 app.listen(3000, () => {
